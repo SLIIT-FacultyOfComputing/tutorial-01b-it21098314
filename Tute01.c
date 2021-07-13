2dd8c6972f0dce2b73ac2c9500703ac92a0ea74c
it21098314 <87365125+it21098314@users.noreply.github.com>
2021-07-13 09:53:27 +0000

    ex 1 complete

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,13 @@
 #include <stdio.h>
 
 int main() {
+  int num1,num2,sum;
+  float avg;
   printf("enter the numbers :");
+  scanf("%d%d",&num1,&num2);
+  sum = num1 + num2;
+  avg = sum/2;
+  printf("average is :%.2f",avg);
   
   return 0;
 }
